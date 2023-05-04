c85bc5070a734bc096e9d0d87b05b9154a73a8f0
halo0516 <ywang20@seas.upenn.edu>
2023-05-04 19:06:15 -0400

    Commit.java implemented
    Commit.java

--- gitlet/Commit.java
+++ gitlet/Commit.java
@@ -1,10 +1,12 @@
 package gitlet;
 
-import java.io.Serializable;
-import java.util.HashMap;
+import java.text.*;
 import java.time.Instant;
-
-import java.util.Date; // TODO: You'll likely use this in this class
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+import java.nio.file.Files;
+import java.util.Date;
 
 /** Represents a gitlet commit object.
  *  TODO: It's a good idea to give a description here of what else this Class
@@ -12,11 +14,28 @@ import java.util.Date; // TODO: You'll likely use this in this class
  *
  *  @author TODO
  */
-public class Commit {
+public class Commit implements Serializable {
+    static File commitPath = new File(System.getProperty("user.dir") + "/.gitlet/Commits");
+    static File serialPath = new File (System.getProperty("user.dir") + "/.gitlet/Serialized");
+    static File stagePath = new File(System.getProperty("user.dir") + "/.gitlet/Staging Area");
+    static File removePath = new File(System.getProperty("user.dir") + "/.gitlet/Removed Files");
+    File prevFile;
+    String logMsg;
+    String timeStamp;
+    String parentHash;
+    String commitHash;
+    TreeSet<String> stageSet = new TreeSet<>();
+    TreeMap<String, String> ptrs;
+    TreeSet removedMark = new TreeSet<>();
+    TreeSet<String> trace = new TreeSet<>();
 
-    private String message;
-    private Instant timestamp;
-    private HashMap<String, String> blobs;
+    public String getMessage() {
+        return "Commit{"
+                + "logMessage= '" + logMsg + '\''
+                + ", timeStamp= " + timeStamp + '\''
+                + ", parentHash= " + parentHash + '\''
+                + '\'' + '}';
+    }
 
     /**
      * TODO: add instance variables here.
@@ -28,25 +47,109 @@ public class Commit {
 
     /** The message of this Commit. */
 
-    public Commit (String message, Instant timestamp, HashMap<String, String> blobs) {
-        this.message = message;
-        this.timestamp = timestamp;
-        this.blobs = blobs;
+    public Commit (String message, Boolean init) {
+        this.logMsg = message;
+        DateFormat dateF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        this.timeStamp = dateF.format(new Date());
+        try {
+            ObjectInputStream input = new ObjectInputStream(
+                new FileInputStream(serialPath + "/pointers.txt"));
+            this.ptrs = (TreeMap) input.readObject();
+            input.close();
+        } catch (IOException | ClassNotFoundException e) {
+            this.ptrs = new TreeMap<>();
+        }
+        if (ptrs.containsKey("HEAD")) {
+            this.parentHash = ptrs.get("HEAD");
+        }
+
+        try {
+            ObjectInputStream input = new ObjectInputStream(
+                new FileInputStream(serialPath + "/tracked.txt"));
+            this.trace = (TreeSet) input.readObject();
+            input.close();
+        } catch(IOException | ClassNotFoundException e) {
+            this.trace = new TreeSet<>();
+        }
+        try {
+            ObjectInputStream input = new ObjectInputStream(
+                new FileInputStream(serialPath + "/removeMark" + ptrs.get("HEAD") +".txt"));
+            this.removedMark = (TreeSet) input.readObject();
+            input.close();
+        }catch (IOException | ClassNotFoundException e) {
+            this.removedMark = new TreeSet<>();
+        }
+        if(!init) {
+            this.prevFile = (new File(new File(System.getProperty("user.dir") + "/.gitlet/Commits") + this.parentHash));
+        }
+        if (stagePath.listFiles().length != 0) {
+            for (File f : stagePath.listFiles()) {
+                stageSet.add(f.getName());
+            }
+        }
     }
 
-    public String getMessage () {
-        return message;
-    }
+    public void commit(boolean init) throws IOException {
+        if (stageSet.isEmpty() && !init && removePath.listFiles().length == 0) {
+            System.out.println("No changes added to the commit.");
+            return;
+        }
+        if (removePath.listFiles().length > 0) {
+            for (File f : removePath.listFiles()) {
+                f.delete();
+            }
+        }
 
-    public Instant getTimestamp () {
-        return timestamp;
-    }
+        if (logMsg == null || logMsg.isEmpty() || logMsg.equals(" ")) {
+            System.out.println("Please enter a commit message.");
+        }
+        commitHash = Utils.sha1(this.getMessage());
+        File newFolder = new File(commitPath, commitHash);
+        newFolder.mkdir();
+        File logMsgFile = new File(commitPath + "/" + commitHash, "/logMsg.txt");
+        Utils.writeContents(logMsgFile, logMsg.getBytes());
+        File timeStampFile = new File(commitPath + "/" + commitHash, "/timeStamp.txt");
+        Utils.writeContents(timeStampFile, timeStamp.getBytes());
+        if (parentHash != null) {
+            File parentHashFile = new File(commitPath + "/" + commitHash, "/parentHash.txt");
+            Utils.writeContents(parentHashFile, parentHash.getBytes());
+        }
+        if (prevFile != null) {
+            for (File f : prevFile.listFiles()) {
+                if (!stageSet.contains(f.getName())
+                    && !(f.getName().equals("logMsg.txt"))
+                    && !(f.getName().equals("timeStamp.txt"))
+                    && !(f.getName().equals("parentHash.txt"))
+                    && !(removedMark.contains(f.getName()))) {
+                    Files.copy(f.toPath(), Paths.get(commitPath + "/"
+                        + commitHash + "/" + f.getName()));
+                }
+            }
+        }
+        if (!stageSet.isEmpty()) {
+            for (File f: stagePath.listFiles()) {
+                f.renameTo(new File(commitPath + "/" + commitHash + "/" + f.getName()));
+                trace.add(f.getName());
+            }
+        }
+        if(!ptrs.isEmpty()) {
+            this.parentHash = ptrs.get(ptrs.get("HEAD"));
+        }
+        if (parentHash == null) {
+            ptrs.put("HEAD", "master");
+            ptrs.put("master", commitHash);
+        } else {
+            ptrs.put(ptrs.get("HEAD"), commitHash);
+        }
 
-    public HashMap<String, String> getBlobs () {
-        return blobs;
-    }
-
-    public String getId() {
-        return Utils.sha1((Object) Utils.serialize((Serializable) this));
+        File ptrsFile = new File(serialPath + "/pointers.txt");
+        try {
+            ObjectOutputStream output = new ObjectOutputStream(
+                new FileOutputStream(ptrsFile));
+            output.writeObject(trace);
+            output.close();
+        } catch (IOException e) {
+            System.out.println("Serialization failed");
+        }
     }
 }
--- gitlet/Main.java
+++ gitlet/Main.java
@@ -18,46 +18,23 @@ public class Main {
      */
     private static final String GITLET_DIR = ".gitlet/";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
         // TODO: what if args is empty?
         //String firstArg = args[0];
         String firstArg = " ";
         switch(firstArg) {
             case "init":
-                init();
                 // TODO: handle the `init` command
                 break;
             case "add":
                 // TODO: handle the `add [filename]` command
                 break;
             // TODO: FILL THE REST IN
-        }
-    }
-
-    private static void init() {
-        Path cwd = Paths.get("").toAbsolutePath();
-        Path gitletDir = cwd.resolve(GITLET_DIR);
-
-        if (Files.exists(gitletDir)) {
-            System.out.println("A gitlet version-control system "
-                    + "already exists in the current directory.");
-            return;
-        }
-
-        try {
-            Files.createDirectories(gitletDir);
-            Commit initCommit = new Commit("initial commit", Instant.ofEpochSecond(0), new HashMap<String, String>());
-            String initCommitId = initCommit.getId();
-            File commitFile = gitletDir.resolve("commit").resolve(initCommitId).toFile();
-            Utils.writeObject(commitFile, initCommitId);
-            HashMap<String, String> commitMap = new HashMap<>();
-            commitMap.put("master",initCommitId);
-            File branchFile = gitletDir.resolve("branch").resolve("master").toFile();
-            Utils.writeObject(branchFile, commitMap);
-            System.out.println("Initialization Complete.");
-        } catch (IOException e) {
-            e.printStackTrace();
+            case "commit":
+                Commit c = new Commit(args[1], false);
+                c.commit(false);
+                break;
         }
     }
 }
